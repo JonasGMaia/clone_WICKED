2b9cbd69658285774f72704cc67cf10d8c74fdf0
Jonas Maia <jonasgmaia2@gmail.com>
2025-06-13 18:27:03 -0300

    home page elements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,15 @@
-function testeapasta(cb){
-    console.log("isso é o sexto teste de js");
-    cb();
-}
+const bgvideo = document.getElementById('bgTitleVideo');
+
+bgvideo.play().catch(error => {
+            console.log("autoplay impedido pelo navegador")});
+
+document.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'visible') {
+        bgvideo.currentTime = 2;
+        bgvideo.play().catch(error => {
+            console.log("autoplay impedido pelo navegador")});
+
+    } else{
+        bgvideo.pause();
+    }
+});
